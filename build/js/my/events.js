2dca7d44876730e19c959aa76d2bbe9e7b56cbff
rshamirza <rshamirza@Rshas-MacBook-Pro.local>
2015-02-21 17:21:47 -0700

    50/50 https://rshamirza.github.io/git-jquery/

--- build/js/my/events.js
+++ build/js/my/events.js
@@ -2,6 +2,27 @@ function myEvents(){
     
     // display a list of events belonging to me in #list
     // e.g., https://api.github.com/users/doubleshow/events
+    console.log('listing my event')    
+    
+    $.get("https://api.github.com/users/rshamirza/events", github, function(data) {
 
-    alert('todo')
+        // Q: What is the parameter 'github'? Where was it defined? What's the purpose? 
+
+        // Q: Why is JSON.parse no longer necessary?
+        var events = data
+        
+        // Q. Why are these templates files stored in a separate folder inside contents/?
+        $.get("/git-jquery/templates/eventList.jade", function(template) {
+
+            // render the template
+            var html = jade.render(template, {items: events})            
+
+            // assign the rendered html to the dom element whose id is #list
+            $("#list").html(html)
+            
+            $("#details").html("")
+
+        })
+
+    })
 }
